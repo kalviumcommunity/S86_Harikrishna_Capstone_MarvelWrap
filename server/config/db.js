9f887cf5c756d3ff0dcdd377fec9854d3989a989
Harry <harikrishnasowreddy14@gmail.com>
2025-05-23 22:10:28 +0530

    db-schemas-setup

--- server/config/db.js
+++ server/config/db.js
@@ -5,10 +5,8 @@ dotenv.config(); // Load .env variables
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/marvelwrap');
+
     console.log('✅ MongoDB connected successfully');
   } catch (err) {
     console.error('❌ MongoDB connection failed:', err.message);
