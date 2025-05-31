595b32d3a201192b5060fff43caeb9ec286eb97f
Harry <harikrishnasowreddy14@gmail.com>
2025-05-31 23:29:04 +0530

    add user authentication

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 const generateToken = (userId) => {
   return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN || '7d',
+    expiresIn: process.env.JWT_EXPIRES_IN || '30d',
   });
 };
 
